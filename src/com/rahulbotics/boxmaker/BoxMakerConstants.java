2012e2dfebb1f5eff4c25ab6041ecdb061ad5157
Robert McGrath <mcgrath@roberts-air.gateway.2wire.net>
2013-12-11 10:51:03 -0600

    Changes made at the Champaign Urbana Community Fab Lab.

--- src/com/rahulbotics/boxmaker/BoxMakerConstants.java
+++ src/com/rahulbotics/boxmaker/BoxMakerConstants.java
@@ -4,6 +4,8 @@ import com.rahulbotics.Constants;
 
 /** 
  * Just a useful place to collect constants used in this application.
+ * <p>
+ * Modified at the Champaign Urbana Community Fab Lab (CUCFL). 
  *
  *	@author rahulb
  */
@@ -11,7 +13,8 @@ public interface BoxMakerConstants extends Constants{
 
 	public static String APP_NAME = "BoxMaker";
 
-	public static String VERSION = "1.6";
+	// [CUCFL] modified
+	public static String VERSION = "1.6CUCFL";
     
     public static String WEBSITE_URL = "http://boxmaker.rahulbotics.com/";
 
--- src/com/rahulbotics/boxmaker/CommandLine.java
+++ src/com/rahulbotics/boxmaker/CommandLine.java
@@ -1,11 +1,14 @@
 package com.rahulbotics.boxmaker;
 
 import java.io.FileNotFoundException;
+import java.io.IOException;
 
 import com.lowagie.text.DocumentException;
 
 /**
  * Simple wrapper to let me run the renderer from the command line
+ * <p>
+ * Modified at the Champaign Urbana Community Fab Lab (CUCFL). 
  * @author rahulb
  */
 public class CommandLine {
@@ -26,19 +29,26 @@ public class CommandLine {
         double mmCutWidth = Double.parseDouble(args[5]);
         double mmNotchLength = Double.parseDouble(args[6]);
         boolean drawBoundingBox = Boolean.parseBoolean(args[7]);
+        boolean hideText = Boolean.parseBoolean(args[8]);  // [CUCFL]
+        boolean epiloglinewidth = Boolean.parseBoolean(args[9]);  // [CUCFL]
+		boolean compact = Boolean.parseBoolean(args[10]);  // [CUCFL]
+		boolean doportrait = Boolean.parseBoolean(args[11]);  // [CUCFL]
         
         // try to render it, don't do any error handling (file won't get created)
         try {
             Renderer.render(filePath,filePath,mmWidth,mmHeight,mmDepth,
                             mmThickness,mmCutWidth,mmNotchLength,
-                            drawBoundingBox,false);
+                            drawBoundingBox,false,hideText,epiloglinewidth,compact,doportrait);
         } catch (FileNotFoundException e) {
             System.out.println("ERROR!"+e.toString());
             System.exit(0);
         } catch (DocumentException e) {
             System.out.println("ERROR!"+e.toString());
             System.exit(0);
-        }
+        } catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
         System.exit(1);
     }
 
